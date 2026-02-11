a0c886e3ca7f7f12ae689a42806f5b3a42109038
alessandromondini@hotmail.com <alessandromondini@hotmail.com>
2026-02-11 16:54:38 +0000

    saved and built int_orders_margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,4 +1,9 @@
 select
-    *,
+    int_sales_margin.orders_id,
+    int_sales_margin.date_date,
+    SUM(int_sales_margin.revenue) AS revenue,
+    SUM(int_sales_margin.quantity) AS quantity,
+    SUM(int_sales_margin.purchase_price) AS purchase_price,
+    SUM(int_sales_margin.margin) AS margin
 from {{ ref('int_sales_margin') }}
-GROUP BY order_id
+GROUP BY int_sales_margin.orders_id, int_sales_margin.date_date
